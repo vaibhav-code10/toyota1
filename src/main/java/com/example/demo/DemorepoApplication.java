c40683a8f12e6d934b78be208486440666690d3f
Vaibhav Srivastava (TCIN) <srivastavavaibhav3v@gmail.com>
2022-08-01 12:49:51 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemorepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemorepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemorepoApplication.class, args);
+	}
+
+}
