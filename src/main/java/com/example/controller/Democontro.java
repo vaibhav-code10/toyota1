66c08647e89954df8fa8dfacbe69d792eb9d2cfe
vaibhav.srivastava <srivastavavaibhav3v@gmail.com>
2022-08-01 14:25:54 +0530

    second change

--- src/main/java/com/example/controller/Democontro.java
+++ src/main/java/com/example/controller/Democontro.java
@@ -1,5 +1,14 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Democontro {
+	
+	@RequestMapping(value="/")
+	public String method1() {
+		return("hi");
+	}
 
 }
